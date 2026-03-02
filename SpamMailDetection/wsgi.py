5c0056c1010c23eb688a5d8c46773fc12457bbbd
srineshr1 <pubgliverisgood@gmail.com>
2026-03-02 10:05:51 +0000

    fix corrupted wsgi.py

--- SpamMailDetection/wsgi.py
+++ SpamMailDetection/wsgi.py
@@ -4,13 +4,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SpamMailDetection.settings')
 
 application = get_wsgi_application()
-cs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SpamMailDetection.settings')
-
-application = get_wsgi_application()
