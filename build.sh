672de189a9cc50e4dd9df2ec2e2ba0c5ed373fcb
srineshr1 <pubgliverisgood@gmail.com>
2026-03-02 11:43:52 +0530

    Upgrade pip and adjust package installation command

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+pip install --upgrade pip
+pip install --prefer-binary -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
